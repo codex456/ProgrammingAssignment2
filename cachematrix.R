e231a53e6444404fafc8b2a650d22352df1364ca
codex456 <pallshah@yahoo.com>
2017-07-04 12:36:27 -0400

    Programming assignment 2
    Create inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+## Create user defined retrivable object. 
 ## functions do
 
-## Write a short comment describing this function
+## makecacheMatrix creates an object that stores user defined matrix
+## and inverses user defined matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  idx <- NULL
+  set <- function(y) {
+    x <<- y
+    idx <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) idx <<- inverse
+  getinverse <- function() idx
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calls inverted matrix stored in makecacheMatrix
+## message is diplayed if data is cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  idx <- x$getinverse()
+  if (!is.null(idx)) {
+    message("getting cached data")
+    return(idx)
+  }
+  data <- x$get()
+  idx <- solve(data, ...)
+  x$setinverse(idx)
+  idx
 }
